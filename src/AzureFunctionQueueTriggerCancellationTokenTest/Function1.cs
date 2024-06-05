bf25afb42ef25006145e6d7cdd9916a7ebb2bef0
Jon Sagara <jonsagara@gmail.com>
2024-06-04 22:15:57 -0700

    Initial commit.

---
+++ src/AzureFunctionQueueTriggerCancellationTokenTest/Function1.cs
@@ -0,0 +1,42 @@
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionQueueTriggerCancellationTokenTest;
+
+public class Function1
+{
+    private readonly ILogger<Function1> _logger;
+
+    public Function1(ILogger<Function1> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(Function1))]
+    public async Task Run([QueueTrigger("test-queue")] QueueMessage message, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
+        _logger.LogInformation("cancellationToken == CancellationToken.None? {IsEqual}", cancellationToken == CancellationToken.None);
+
+        cancellationToken.Register(() =>
+        {
+            Console.WriteLine("CancellationToken Register callback invoked.");
+        });
+
+        foreach (var ix in Enumerable.Range(start: 0, count: 1_000))
+        {
+            await Task.Delay(1_000);
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Cancellation requested!");
+                return;
+            }
+            else
+            {
+                _logger.LogInformation("Cancellation not requested.");
+            }
+        }
+    }
+}
